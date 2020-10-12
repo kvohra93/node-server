817c2f7a35fe1b6ec5994c2d8104f2f3e6288463
Kunal <70285661+kvohra93@users.noreply.github.com>
2020-10-11 19:47:42 -0500

    data.json added
    added data to return in response

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const http = require('http')  // require built-in Node.js http package
-
+const http = require('http');  // require built-in Node.js http package
+const fs = require('fs');
 // Use hosting values if available, otherwise default 
-const hostname = process.env.hostname || '0.0.0.0' // allow remote access
-const port = process.env.PORT || 3002
+const hostname = process.env.hostname || '0.0.0.0'; // allow remote access
+const port = process.env.PORT || 3002;
+
+fs.readFile('data.json', function (err, data) {
+    if (err) {
+        console.log('error!');
+        throw err;
+    } 
 
 // define our server
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ a: 1 }));
+  res.end(JSON.stringify({data}));
 })
 
 // start listening
