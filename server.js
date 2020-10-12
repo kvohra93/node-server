96b1155ef3f1dd9cf41159aee43952baece68097
Kunal <70285661+kvohra93@users.noreply.github.com>
2020-10-11 21:44:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http = require('http');  // require built-in Node.js http package
+const http = require('http');  
 const fs = require('fs');
-// Use hosting values if available, otherwise default 
-const hostname = process.env.hostname || '0.0.0.0'; // allow remote access
+
+const hostname = process.env.hostname || '0.0.0.0'; 
 const port = process.env.PORT || 3000;
 
 fs.readFile('data.json', function (err, data) {
@@ -9,13 +9,16 @@ fs.readFile('data.json', function (err, data) {
         console.log('error!');
         throw err;
     } 
-
-// define our server
-http.createServer(function(_req, res) {
-  
-  res.setHeader(200,{"Content-Type": "application/json"});
-  res.write(JSON.stringify(data))
-  console.log(`Server running at http://${hostname}:${port}/`);
-  res.end();
-}).listen(port, hostname);
+    const server = http.createServer ((req, res) => {
+        res.setHeader(200,{"Content-Type": "application/json"})
+        res.write(JSON.stringify(data))
+        res.end();
 })
+
+
+    server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+})
+
+
